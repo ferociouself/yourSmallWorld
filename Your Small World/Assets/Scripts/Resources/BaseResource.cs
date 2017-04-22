d816fcd667472ab55dcabe2b6969de8effa6d83a
zsd4yr <zsd4yr@virginia.edu>
2017-04-21 22:31:00 -0400

    some basics

---
+++ Your Small World/Assets/Scripts/Resources/BaseResource.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseResource : MonoBehaviour {
+
+	List<BaseResource> prereqs;
+	bool unlocked;
+
+	void Awake(){
+		prereqs = new List<BaseResource> ();
+		unlocked = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Unlock(){
+		unlocked = true;
+	}
+
+	public List<BaseResource> GetPrereqs(){
+		return prereqs;
+	}
+}
