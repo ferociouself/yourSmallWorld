55c57f0f1573e9e23d3a095a026d54d3e435e98b
Soupdoop <jherd23@gmail.com>
2017-04-22 12:57:12 -0400

    Sphere Bulging

---
+++ Your Small World/Assets/Scripts/Terrain/SphereTerrain.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+public class SphereTerrain : MonoBehaviour {
+
+	//private List<QuadTree> sides;
+	public int radius;
+
+	// Use this for initialization
+	void Start () {
+		/*sides = new List<QuadTree> ();
+		sides.Add (new QuadTree (gameObject.transform.position + Vector3.down, 1, Vector3.right, Vector3.forward));
+		sides.Add (new QuadTree (gameObject.transform.position + Vector3.up, 1, Vector3.right, Vector3.forward));
+		sides.Add (new QuadTree (gameObject.transform.position + Vector3.back, 1, Vector3.right, Vector3.up));
+		sides.Add (new QuadTree (gameObject.transform.position + Vector3.forward, 1, Vector3.right, Vector3.up));
+		sides.Add (new QuadTree (gameObject.transform.position + Vector3.left, 1, Vector3.forward, Vector3.up));
+		sides.Add (new QuadTree (gameObject.transform.position + Vector3.right, 1, Vector3.forward, Vector3.up));*/
+		radius = 10;
+		updateMesh ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updateMesh() {
+		Mesh planetMesh = GetComponent<MeshFilter> ().mesh;
+		planetMesh.vertices = expandCube (planetMesh.vertices, new float[]{1, 2, 3, -1, -2, 0, 1});
+	}
+
+	/*public Vector3[] vertices() {
+		List<Vector3> vertexes = new List<Vector3> ();
+		foreach (QuadTree q in sides) {
+			vertexes.AddRange (q.getSphericalVertices (4, gameObject.transform.position, 1));
+		}
+		return vertexes.ToArray ();
+	}*/
+
+	public Vector3[] expandCube(Vector3[] cubeVertices, float[] heightmap) {
+		Vector3[] endVertices = new Vector3[cubeVertices.Length];
+		for (int i = 0; i < cubeVertices.Length; i++) {
+			endVertices [i] = (cubeVertices [i] - gameObject.transform.position) * (radius + heightmap [i % heightmap.Length]);
+		}
+		return endVertices;
+	}
+}
