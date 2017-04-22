096a2d0ce58076d682ca5c323f8edf788aadebaa
zsd4yr <zsd4yr@virginia.edu>
2017-04-21 23:51:54 -0400

    oops

--- Your Small World/Assets/Scripts/Resources/Craftable/Copper.cs
+++ Your Small World/Assets/Scripts/Resources/Craftable/Copper.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Copper : BaseResource {
+public class Copper : CraftableResource {
 
 	// Use this for initialization
 	void Start () {
