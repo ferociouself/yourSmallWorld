fe93cd246de1ad622926a48a79174fd3640d305a
Soupdoop <jherd23@gmail.com>
2017-04-22 18:18:16 -0400

    Som Whater

---
+++ Your Small World/Assets/Scripts/Terrain/GravityAffected.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityAffected : MonoBehaviour {
+
+	public GameObject centerOfGravity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Rigidbody> ().AddExplosionForce (-1, centerOfGravity.transform.position, 100);
+		if ((this.gameObject.transform.position - centerOfGravity.transform.position).magnitude < 0.5f) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Your Small World/Assets/Scripts/Terrain/Spring.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour {
+
+	public float Rate;
+	public GameObject planet;
+	float delay;
+
+	// Use this for initialization
+	void Start () {
+		delay = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (delay >= 1 / Rate) {
+			makeWater ();
+			delay = 0;
+		}
+		delay += Time.deltaTime;
+	}
+
+	void makeWater() {
+		GameObject waterball = Resources.Load ("prefabs/water") as GameObject;
+		GameObject realBall = GameObject.Instantiate (waterball);
+		realBall.transform.position = this.transform.position;
+		realBall.GetComponent<GravityAffected> ().centerOfGravity = planet;
+	}
+}
