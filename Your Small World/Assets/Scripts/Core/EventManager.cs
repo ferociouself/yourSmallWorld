167779c39f5be99df51ea28503f1e16a200de9ae
zsd4yr <zsd4yr@virginia.edu>
2017-04-22 22:34:21 -0400

    syntax motherfucker

--- Your Small World/Assets/Scripts/Core/EventManager.cs
+++ Your Small World/Assets/Scripts/Core/EventManager.cs
@@ -9,32 +9,23 @@ public class EventManager : MonoBehaviour {
 
 	private static EventManager eventManager;
 
-	public static EventManager instance
-	{
-		get
-		{
-			if (!eventManager)
-			{
+	public static EventManager instance {
+		get {
+			if (!eventManager){
 				eventManager = FindObjectOfType (typeof (EventManager)) as EventManager;
-
-				if (!eventManager)
-				{
+				if (!eventManager){
 					Debug.LogError ("There needs to be one active EventManger script on a GameObject in your scene.");
-				}
-				else
-				{
+				} else {
 					eventManager.Init (); 
 				}
 			}
-
 			return eventManager;
 		}
 	}
 
 	void Init ()
 	{
-		if (eventDictionary == null)
-		{
+		if (eventDictionary == null) {
 			eventDictionary = new Dictionary<string, UnityEvent>();
 		}
 	}
@@ -42,12 +33,9 @@ public class EventManager : MonoBehaviour {
 	public static void StartListening (string eventName, UnityAction listener)
 	{
 		UnityEvent thisEvent = null;
-		if (instance.eventDictionary.TryGetValue (eventName, out thisEvent))
-		{
+		if (instance.eventDictionary.TryGetValue (eventName, out thisEvent)) {
 			thisEvent.AddListener (listener);
-		} 
-		else
-		{
+		} else {
 			thisEvent = new UnityEvent ();
 			thisEvent.AddListener (listener);
 			instance.eventDictionary.Add (eventName, thisEvent);
@@ -58,8 +46,7 @@ public class EventManager : MonoBehaviour {
 	{
 		if (eventManager == null) return;
 		UnityEvent thisEvent = null;
-		if (instance.eventDictionary.TryGetValue (eventName, out thisEvent))
-		{
+		if (instance.eventDictionary.TryGetValue (eventName, out thisEvent)) {
 			thisEvent.RemoveListener (listener);
 		}
 	}
@@ -67,8 +54,7 @@ public class EventManager : MonoBehaviour {
 	public static void TriggerEvent (string eventName)
 	{
 		UnityEvent thisEvent = null;
-		if (instance.eventDictionary.TryGetValue (eventName, out thisEvent))
-		{
+		if (instance.eventDictionary.TryGetValue (eventName, out thisEvent)) {
 			thisEvent.Invoke ();
 		}
 	}
