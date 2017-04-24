afe81b913ed0bb6e7ef7b2b3778a99ecb246f716
ferociouself <jhe3cd@virginia.edu>
2017-04-23 23:51:11 -0400

    100% bruh

--- Your Small World/Assets/Scripts/Core/ResourceController.cs
+++ Your Small World/Assets/Scripts/Core/ResourceController.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ResourceController : MonoBehaviour {
 
+	UnityAction action;
+
 	// Use this for initialization
 	void Start () {
-		
+		action = new UnityAction(WaterMade);
+		EventManager.StartListening("Resource", action);
 	}
 	
 	// Update is called once per frame
@@ -15,8 +19,13 @@ public class ResourceController : MonoBehaviour {
 	}
 
 	public void ResourceMade(BaseResource b, Vertex v) {
+		Debug.Log("Resource made");
 		if (GetComponent<TierController>().CheckIfWant(b)) {
 			GetComponent<Community>().SendBoiToGood(b, v);
 		}
+	}
+
+	void WaterMade() {
+		Debug.Log("Water Made");
 	}
 }
