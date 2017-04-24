75d1ca89499b89d39ce337d7533ca5c2cff6ee9e
zsd4yr <zsd4yr@virginia.edu>
2017-04-24 02:17:40 -0400

    the 7th child of the 7th child

--- Your Small World/Assets/Scripts/nGameResources/ResourceFooBar.cs
+++ Your Small World/Assets/Scripts/nGameResources/ResourceFooBar.cs
@@ -16,42 +16,42 @@ public class ResourceFooBar : MonoBehaviour {
 	}
 
 	public void UnlockWater(){
-		gameObject.transform.GetChild (0).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (0).gameObject.GetComponent<Button> ().interactable = true;
 	}
 
 	public void UnlockStone(){
-		gameObject.transform.GetChild (1).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (1).gameObject.GetComponent<Button> ().interactable = true;
 	}
 
 	public void UnlockSand(){
-		gameObject.transform.GetChild (2).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (2).gameObject.GetComponent<Button> ().interactable = true;
 	}
 
 	public void UnlockTree(){
-		gameObject.transform.GetChild (3).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (3).gameObject.GetComponent<Button> ().interactable = true;
 	}
 
 	public void UnlockWheat(){
-		gameObject.transform.GetChild (4).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (4).gameObject.GetComponent<Button> ().interactable = true;
 	}
 
 	public void UnlockOil(){
-		gameObject.transform.GetChild (5).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (5).gameObject.GetComponent<Button> ().interactable = true;
 	}
 
 	public void UnlockIron(){
-		gameObject.transform.GetChild (6).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (6).gameObject.GetComponent<Button> ().interactable = true;
 	}
 
 	public void UnlockCopper(){
-		gameObject.transform.GetChild (7).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (7).gameObject.GetComponent<Button> ().interactable = true;
 	}
 
 	public void UnlockCoal(){
-		gameObject.transform.GetChild (8).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (8).gameObject.GetComponent<Button> ().interactable = true;
 	}
 
 	public void UnlockDieton(){
-		gameObject.transform.GetChild (9).gameObject.GetComponent<Button> ().interactable = true;
+		gameObject.transform.GetChild (7).GetChild (9).gameObject.GetComponent<Button> ().interactable = true;
 	}
 }
