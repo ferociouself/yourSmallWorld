f0adc41a0ef6936f84addef9f662311f062fd8c9
zsd4yr <zsd4yr@virginia.edu>
2017-04-24 00:36:56 -0400

    m

---
+++ Your Small World/Assets/Scripts/Core/clickedbook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickedbook : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ClickedBook(){
+		Debug.Log ("you clicked the book!");
+		this.gameObject.GetComponent<AudioSource> ().Play ();
+	}
+}
